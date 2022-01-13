148cb3a4bf834b0f92228f3fe1f7ffee63d85bb9
ItsMeJules <jules.prn06@gmail.com>
2022-01-13 10:25:40 +0100

    ex01 finished

---
+++ module06/ex01/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+typedef struct	s_data {
+	int age;
+	double tempC;
+	float height;
+	std::string name;
+}				Data;
+
+uintptr_t serialize(Data *data) {
+	return reinterpret_cast<uintptr_t>(data);
+}
+
+Data *deserialize(uintptr_t raw) {
+	return reinterpret_cast<Data *>(raw);
+}
+
+void printStruct(Data *data) {
+	std::cout << "data->age = " << data->age << std::endl;
+	std::cout << "data->tempC = " << data->tempC << std::endl;
+	std::cout << "data->height = " << data->height << std::endl;
+	std::cout << "data->name = " << data->name << std::endl;
+}
+
+int main() {
+	Data *dataPtr = new Data;
+	dataPtr->age = 21;
+	dataPtr->tempC = 8.4;
+	dataPtr->height = 180.3;
+	dataPtr->name = "Jules";
+	printStruct(dataPtr);
+
+	uintptr_t uPtr = serialize(dataPtr);
+	std::cout << std::endl;
+	std::cout << "Address of dataPtr " << dataPtr << std::endl;
+	std::cout << "Address of uPtr " << &uPtr << std::endl;
+
+	Data *deserializedDataPtr = deserialize(uPtr);
+	std::cout << std::endl;
+	printStruct(deserializedDataPtr);
+	std::cout << std::endl;
+	std::cout << "Address of deserializedDataPtr " << &deserializedDataPtr << std::endl;
+	std::cout << "Address of uPtr " << &uPtr << std::endl;
+	return 1;
+}
