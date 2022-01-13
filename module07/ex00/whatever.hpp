e21d76a25a89ce2e5929c3c9c51ac35dd74fc8e3
ItsMeJules <jules.prn06@gmail.com>
2022-01-13 18:09:01 +0100

    Finished ex00

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,23 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template<typename T>
+void swap(T &a, T &b) {
+    T c;
+
+    c = a;
+    a = b;
+    b = c;
+}
+
+template<typename T>
+T const &min(T const &a, T const &b) {
+    return a > b ? b : a;
+}
+
+template<typename T>
+T const &max(T const &a, T const &b) {
+    return a > b ? a : b;
+}
+
+#endif
