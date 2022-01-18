76b2e88866ed5a9108aabe17208410ef919bdd96
ItsMeJules <jules.prn06@gmail.com>
2022-01-18 18:39:38 +0100

    Started module08/ex01

---
+++ module08/ex01/span.cpp
@@ -0,0 +1,29 @@
+#include "span.hpp"
+
+span::span() {
+
+}
+
+span::span(unsigned int N) {
+    
+}
+
+span::span(span const &span) {
+    
+}
+
+span::~span() {
+    
+}
+
+void span::addNumber(int n) {
+
+}
+
+int span::shortestSpan() const {
+
+}
+
+int span::longestSpan() const {
+    
+}
---
+++ module08/ex01/span.hpp
@@ -0,0 +1,28 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <vector>
+# include <algorithm>
+
+class span {
+    private:
+        unsigned int size_;
+        std::vector<int> vector;
+    public:
+        span();
+        span(unsigned int N);
+        span(span const &span);
+        ~span();
+
+        void addNumber(int n);
+        void addRange(std::vector<int>::iterator begin, std::vector<int>::iterator);
+        int shortestSpan() const;
+        int longestSpan() const;
+
+        unsigned int getSize() const;
+        std::vector<int> const &getVector() const;
+
+        span &operator=(span const &rhs);
+};
+
+#endif
